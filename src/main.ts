f45e9ff123bb0ab3f1a784f27b3e72cd8d3a7b32
Roberto Ceccato <roberto@vivaldigroup.it>
2025-07-22 17:46:59 +0200

    change logs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { HttpServerController } from './controllers/httpserver.controller.ts';
 import { DatabaseController } from './controllers/db.controller.ts';
 import { WSClientController } from './controllers/ws-client.controller.ts';
 
+console.log("Music FestOn");
+console.log("Backend per gestione ordini e stampa scontrini su stampanti termiche Ethernet");
+
 // Inizializza il controller del database
 const dbController = DatabaseController.instance;
 // Inizializza il server HTTP
